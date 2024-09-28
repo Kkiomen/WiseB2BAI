5152dd1595f8d4c749001856342dd5a1399ab12a
jowsianka <kuba.owsianka@interia.pl>
2024-09-29 00:26:19 +0200

    Prototype Fields correct find

--- app/Core/AssistantMessage/Prompts/TaxAssistantPromptHelper.php
+++ app/Core/AssistantMessage/Prompts/TaxAssistantPromptHelper.php
@@ -3,6 +3,7 @@
 namespace App\Core\AssistantMessage\Prompts;
 
 use App\Core\AssistantMessage\Enums\DirectionOfConversationEnum;
+use Illuminate\Support\Facades\Storage;
 
 class TaxAssistantPromptHelper
 {
@@ -38,6 +39,26 @@ class TaxAssistantPromptHelper
 
         $prompt =
             'Na podstawie treści jaką podał użytkownik określ jaki jest cel konwersacji. ### Dostępne cele: ' . $directionConversation . '### Możesz jedynie zwrócić nazwę celu i nic więcej';
+
+        return $prompt;
+    }
+
+
+    public static function getPromptToReturnListFieldsToFill(): string
+    {
+        $data = Storage::get('declaration_fields/pcc-3/fields.json');
+        $data = json_decode($data, true);
+        $dataResult = [];
+        $result = '[';
+        foreach ($data as $field){
+            $dataResult[] = '{field_number: "'.$field['number'].'", field_name: "'.$field['name'].'", field_type: "'.$field['type'].'", field_required: "'.$field['required'].'", field_description: "'.$field['description'] . ' ' . $field['rules'].'"" }';
+        }
+        $result .= implode(', ', $dataResult);
+        $result .= ']';
+
+
+        $prompt =
+            'Jesteś asystentem deklaracji podatkowej. Twoim zadaniem jest identyfikacja wymaganych pól na podstawie opisu użytkownika. Otrzymujesz informacje o polach w deklaracji, w tym ich numer, nazwę, typ danych, czy są wymagane, opis i zasady wypełniania. Na podstawie tych informacji oraz kontekstu transakcji opisanego przez użytkownika, określ listę pól, które muszą być wypełnione, oraz dodatkowe informacje, takie jak stawki podatku. Weź pod uwagę pola wymagane jak i nie wymagane (jeśli istnieją pod sytuacje użytkownika). Uwzględnij instrukcje w "rules". Zwróć uwagę, że field ma pole rules, które również steruje czy pole ma zostać wyświetlone ### Pola: ' . $result . '### Oczekiwany Output: Wymieniasz wszystkie wymagane pola, które użytkownik musi wypełnić. Zwróc mi odpowiedź w formacie JSON i tylko JSON (Bez markdown) [{\'field_number\': \'XYZ\'},...]';
 
         return $prompt;
     }
